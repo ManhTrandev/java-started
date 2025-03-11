44d199853f1954b3a9b039b25f0b3d7875755412
ManhTrandev <manhttran121@gmail.com>
2025-03-11 15:12:04 +0700

    java scanner

--- src/javaintern/HelloWorld.java
+++ src/javaintern/HelloWorld.java
@@ -1,12 +1,15 @@
 package javaintern;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World with JAVA");
-		System.out.println("Hello World with JAVA");
-		System.out.println("Hello World with JAVA");
-		System.out.println("Hello World with JAVA");
-		System.out.println("Hello World with JAVA");
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
 	}
 }
