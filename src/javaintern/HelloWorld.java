417aa542f03264928d63f8dc3876c0ea3720d51a
ManhTrandev <manhttran121@gmail.com>
2025-03-11 14:39:20 +0700

    init project

---
+++ src/javaintern/HelloWorld.java
@@ -0,0 +1,12 @@
+package javaintern;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with JAVA");
+		System.out.println("Hello World with JAVA");
+		System.out.println("Hello World with JAVA");
+		System.out.println("Hello World with JAVA");
+		System.out.println("Hello World with JAVA");
+
+	}
+}
