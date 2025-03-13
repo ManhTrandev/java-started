e6ad992fc2f55b935dd393c7a7a2d3003461d831
ManhTrandev <manhttran121@gmail.com>
2025-03-13 14:27:10 +0700

    package test

---
+++ src/p1/P1.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class P1 {
+	public static void main(String[] agrs) {
+		P firstObj = new P();
+		firstObj.a = 10;
+		firstObj.b = 10;
+
+		firstObj.c = 10;
+
+		// firstObj.d = 10; >>> private
+
+	}
+}
---
+++ src/p2/P2.java
@@ -0,0 +1,16 @@
+package p2;
+
+import p1.P;
+
+public class P2 {
+	public static void main(String[] agrs) {
+		P firstObj = new P();
+		firstObj.a = 10;
+		// firstObj.b = 10;
+
+		// firstObj.c = 10;
+
+		// firstObj.d = 10; >>> private
+
+	}
+}
