084a2fd333c99220c07fcfb7f0eebeed3e98ef96
ManhTrandev <manhttran121@gmail.com>
2025-03-13 14:01:30 +0700

    chapter 4

---
+++ src/chapter4/checkst.java
@@ -0,0 +1,9 @@
+package chapter4;
+
+public class checkst {
+	public static void main(String[] agrs) {
+		student st1 = new student();
+		student st2 = new student("Manh", 24);
+		System.out.println("Ten" + st2.name + "tuoi" + st2.age);
+	}
+}
---
+++ src/chapter4/student.java
@@ -0,0 +1,23 @@
+package chapter4;
+
+public class student {
+	String name;
+	int age;
+
+	public student() {
+
+	}
+
+	public student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void learnJava() {
+		System.out.println("Hoc java");
+	}
+
+	String getAge() {
+		return "ManhTran";
+	}
+}
