a61210a39dda38b9fb7f30afe56589a39fb06a2c
ManhTrandev <manhttran121@gmail.com>
2025-03-18 11:56:59 +0700

    java io

---
+++ src/chapter10/Video59.java
@@ -0,0 +1,30 @@
+package chapter10;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Video59 {
+	public static void main(String[] agrs) throws IOException {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+
+		try {
+			in = new FileInputStream("test.txt");
+			out = new FileOutputStream("outagain.txt");
+			int c;
+
+			while ((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
+		System.out.println("END");
+	}
+}
